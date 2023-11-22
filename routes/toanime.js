cb37d63089901a47892c239746ad0a327f712c68
MikoNeko <142219446+Miko7799@users.noreply.github.com>
2023-11-22 18:39:20 +0700

    Update toanime.jshttps://tools.betabotz.org

--- routes/toanime.js
+++ routes/toanime.js
@@ -1,55 +1,57 @@
 const express = require('express'); 
- const router = express.Router(); 
- const axios = require('axios'); 
+const router = express.Router(); 
+const axios = require('axios'); 
 const Jimp = require('jimp');
 const FormData = require('form-data');
 const baseUrl = 'https://tools.betabotz.org';
 
-  
- router.get('/', async (req, res) => { 
-   try { 
-     res.setHeader('Content-Type', 'application/json'); 
-   const URL = req.query.url;
-     const image = await Jimp.read(URL);
-      const buffer = await new Promise((resolve, reject) => {
-    image.getBuffer(Jimp.MIME_JPEG, (err, buf) => {
-      if (err) {
-        reject('Terjadi Error Saat Mengambil Data......');
-      } else {
-        resolve(buf);
-      }
+router.get('/', async (req, res) => { 
+  try { 
+    res.setHeader('Content-Type', 'application/json'); 
+    const URL = req.query.url;
+    const image = await Jimp.read(URL);
+    const buffer = await new Promise((resolve, reject) => {
+      image.getBuffer(Jimp.MIME_JPEG, (err, buf) => {
+        if (err) {
+          reject('Terjadi Error Saat Mengambil Data......');
+        } else {
+          resolve(buf);
+        }
+      });
     });
-  });
-  const form = new FormData();
-  form.append('image', buffer, { filename: 'toanime.jpg' });
-  
-  try {
-    const { data } = await axios.post(`${baseUrl}/ai/toanime`, form, {
-      headers: {
-        ...form.getHeaders(),
-        'accept': 'application/json',
-      },
-    });
-    var res = {
-      image_data: data.result,
-      image_size: data.size
-    };
-    return res;
-  } catch (error) {
-    console.error('Identifikasi Gagal:', error);
-    return 'Identifikasi Gagal';
-  }
-     const dataimg = image.image_data; 
-     const imageUrl = dataimg
-     const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' }); 
-     const imageBuffer = Buffer.from(imageResponse.data, 'binary'); 
-     res.setHeader('Content-Type', 'image/jpeg'); 
-     res.send(imageBuffer); 
-   } catch (error) { 
-     console.error(error); 
-     res.status(500).send('An error occurred'); 
-   } 
- }); 
-  
- module.exports = router;
- 
+
+    const form = new FormData();
+    form.append('image', buffer, { filename: 'toanime.jpg' });
+
+    try {
+      const { data } = await axios.post(`${baseUrl}/ai/toanime`, form, {
+        headers: {
+          ...form.getHeaders(),
+          'accept': 'application/json',
+        },
+      });
+
+      var result = {
+        image_data: data.result,
+        image_size: data.size
+      };
+
+      const dataimg = result.image_data; 
+      const imageUrl = dataimg;
+      const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' }); 
+      const imageBuffer = Buffer.from(imageResponse.data, 'binary'); 
+      res.setHeader('Content-Type', 'image/jpeg'); 
+      res.send(imageBuffer);
+
+      return result;
+    } catch (error) {
+      console.error('Identifikasi Gagal:', error);
+      return 'Identifikasi Gagal';
+    }
+  } catch (error) { 
+    console.error(error); 
+    res.status(500).send('An error occurred'); 
+  } 
+});
+
+module.exports = router;
